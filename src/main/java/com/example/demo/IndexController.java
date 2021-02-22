01932090426251031eaa09007353929d69bbb700
yummy-bear <mudskipper7ki@gmail.com>
2021-02-22 20:05:03 +0900

    Third

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -10,14 +10,18 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexController {
 	@RequestMapping(value="/num", method=RequestMethod.GET)
 	public ModelAndView indexGET(ModelAndView mv) {
-		mv.addObject("num", "半角で入力");
+		mv.addObject("num", "階乗の計算をします");
 		mv.setViewName("index");
 		return mv;
 	}
 	
 	@RequestMapping(value="/num", method=RequestMethod.POST)
-	public ModelAndView indexPOST(ModelAndView mv, @RequestParam("numVal")String num) {
-	  mv.addObject("num", num);
+	public ModelAndView indexPOST(ModelAndView mv, @RequestParam("numVal") int num) {
+		int factorial = 1;
+		for(int i = 0; num > i; i++) {
+			factorial *= num - i ;
+		}
+	  mv.addObject("num", num + "の階乗は" + factorial + "です");
 	  mv.setViewName("index");
 	  return mv;
 	}
